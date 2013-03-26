4112bdc69fa37ea9f09910682047565d807beaa3
Parker Kemp <charles.kemp702@topper.wku.edu>
2013-03-25 20:59:30 -0500

    Test

--- Astar/AStar.mm
+++ Astar/AStar.mm
@@ -14,6 +14,7 @@ void Anode::init(Astar *a, location l, Anode *p){
 	calculateG();
 	}
 
+
 void Anode::calculateH(){
 	location d = astar->destination();
 	hCost = abs(d.x - _loc.x) + abs(d.y - _loc.y);
